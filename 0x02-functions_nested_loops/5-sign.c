363c74be332df9b7899e4c38227ac0b8c9b517e4
ghizooghizoo <benmoumenghizlane55@gmail.com>
2023-03-21 16:07:33 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sing - function that prints the sing of a number
+ *
+ * @n: integer
+ *
+ * Return: 1 is greater than zero, 0 is zero, -1 is less than zero
+ */
+int print_sing(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('_');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('\n');
+}
