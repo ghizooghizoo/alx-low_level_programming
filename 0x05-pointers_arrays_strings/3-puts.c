a95cfad97b21f8f1fc27c620e1a0776cde93d970
ghizooghizoo <benmoumenghizlane55@gmail.com>
2023-03-27 15:55:39 -0700

    main.h

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,20 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
  * _puts - prints a string
- * @str: tring
+ * @str: the string to print
+ * Return: the length of the string
  */
 
 void _puts(char *str)
 {
-	while (*str != '\0')
-		_putchar(*str++);
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,5 +14,6 @@ void puts(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _putchar(char c);
 
 #endif
