5a013342eefb7ff417e91318a82bd9df9706999a
ghizooghizoo <benmoumenghizlane55@gmail.com>
2023-03-29 16:49:11 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *str)
 
 	while (*(str + i))
 	{
-		if (*(str + 1) >= 'a' && *(str + i) <= 'z')
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
 		{
 			if (i == 0)
 				*(str + i) -= 'a' - 'A';
