a9e9ee42899472d15ea2d375c5f19eb1ac3be1ea
ghizooghizoo <benmoumenghizlane55@gmail.com>
2023-04-19 15:05:01 -0700

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,14 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * int _index - searches for integer
+ * int_index - searches for integer
  * @array: the int array
  * @size: the array size
  * @cmp: the compare function
  *
  * Return: the integer index
  */
-int int_index(int *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int(*cmp)(int))
 {
 	int i = 0;
 
