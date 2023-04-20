3e62fdf41e9453cd6bdd9ef37b03ef0d4291dd1e
ghizooghizoo <benmoumenghizlane55@gmail.com>
2023-04-19 17:58:45 -0700

    m

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,60 +7,60 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 /**
- * op_add - adds two integers
- * @a: the first int
- * @b: the second int
- *
- * Return: the sum
+ * op_add - adds 2 int
+ * @a: int
+ * @b: int
+ * Return: sum
  */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - subtracts two integers
- * @a: the first int
- * @b: the second int
- *
- * Return: the difference
+ * op_sub - subtracts 2 int
+ * @a: int
+ * @b: int
+ * Return: difference
  */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - multiplies two integers
- *@a: the first int
- *@b: the second int
- *
- * Return: the product
+ * op_mul - multiplies 2 int
+ * @a: int
+ * @b: int
+ * Return: product
  */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - divides two integers
- * @a: the first int
- * @b: the second int
- *
- * Return: quotient
+ * op_div - divides 2 int
+ * @a: int
+ * @b: int
+ * Return: the result
  */
+
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
 /**
- * op_mod - modulo two integers
- * @a: the first int
- * @b: the second int
- *
+ * op_mod - modulo 2 int
+ * @a: int
+ * @b: int
  * Return: the remainder
  */
+
 int op_mod(int a, int b)
 {
 	return (a % b);
