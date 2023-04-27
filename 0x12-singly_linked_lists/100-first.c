b65b3f39a3571df2740148dcfbd9f91e13a100f2
ghizooghizoo <benmoumenghizlane55@gmail.com>
2023-04-27 10:07:23 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,5 @@ void _constructor(void) __attribute__ ((constructor));
  */
 void _constructor(void)
 {
-	printf("you're beat! and yet, you must allow,\n");
-			printf("I bore my house upon my back!\n");
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 }
