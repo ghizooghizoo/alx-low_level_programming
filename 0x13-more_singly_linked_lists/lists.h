e85aeab2bc4ee99bd49d599bda731d33868c8e34
ghizooghizoo <benmoumenghizlane55@gmail.com>
2023-04-30 19:05:07 -0700

    lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,18 +6,18 @@
 #include <string.h>
 
 /**
- * strurct listins_s - singly linked list
+ * strurct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Desctiption: singly linked list node structure
+ *
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
-
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
