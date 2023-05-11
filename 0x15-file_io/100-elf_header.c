1522ddf6fd51ae07ec282f70fa54dbc63383da92
ghizooghizoo <benmoumenghizlane55@gmail.com>
2023-05-11 12:08:11 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -2,43 +2,43 @@
 #include "elf_a.h"
 
 /**
- * print_version - Prints the version of an ELF header
- * @e_ident: A pointer to an array containing the ELF version
+ * print_version - Prints the version of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF version.
  */
 void print_version(unsigned char *e_ident)
 {
-	printf("  Version:                        %d",
-			e_ident[EI_VERSION]);
+	printf("  Version:                           %d",
+	       e_ident[EI_VERSION]);
 
 	switch (e_ident[EI_VERSION])
 	{
-		case EV_CURRENT:
-			printf(" (current)\n");
-			break;
-		default:
-			printf("\n");
-			break;
+	case EV_CURRENT:
+		printf(" (current)\n");
+		break;
+	default:
+		printf("\n");
+		break;
 	}
 }
 /**
- * close_elf - Closes an ELF file
- * @elf: The file descriptor of the ELF file
+ * close_elf - Closes an ELF file.
+ * @elf: The file descriptor of the ELF file.
  */
 void close_elf(int elf)
 {
 	if (close(elf) == -1)
 	{
 		dprintf(STDERR_FILENO,
-				"Error: Can't close fd %d\n", elf);
+		"Error: Can't close fd %d\n", elf);
 		exit(98);
 	}
 }
 /**
  * main - Displays the information contained in the
- * ELF header at the start of an ELF file
- * @argc: The number of arguments supplied to the program
- * @argv: An array of pointers to the arguments
- * Return: 0 on success
+ *	ELF header at the start of an ELF file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: 0 on success.
  */
 int main(__silent int argc, char *argv[])
 {
